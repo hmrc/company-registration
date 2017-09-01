945a91da133031b4c90b965bb91c67ef343cd478
Les Jones <les.jones@digital.hmrc.gov.uk>
2017-09-01 12:54:01 +0100

    SCRS-8208 added test to prove for incorp status calls still work

---
+++ it/connectors/IncorporationCheckAPIConnectorISpec.scala
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import itutil.{IntegrationSpecBase, WiremockHelper}
+import models._
+import org.joda.time.format.DateTimeFormat
+import org.joda.time.DateTime
+import play.api.Application
+import play.api.inject.guice.GuiceApplicationBuilder
+import uk.gov.hmrc.play.http.HeaderCarrier
+
+class IncorporationCheckAPIConnectorISpec extends IntegrationSpecBase {
+  val mockHost = WiremockHelper.wiremockHost
+  val mockPort = WiremockHelper.wiremockPort
+  val mockUrl = s"http://$mockHost:$mockPort"
+
+  val additionalConfiguration = Map(
+    "auditing.consumer.baseUri.host" -> s"$mockHost",
+    "auditing.consumer.baseUri.port" -> s"$mockPort",
+    "microservice.services.company-registration-frontend.host" -> s"$mockHost",
+    "microservice.services.company-registration-frontend.port" -> s"$mockPort"
+  )
+
+  override implicit lazy val app: Application = new GuiceApplicationBuilder()
+    .configure(additionalConfiguration)
+    .build()
+
+  implicit val hc = HeaderCarrier()
+
+  def asDate(date: String) = DateTime.parse(date, DateTimeFormat.forPattern("yyyy-MM-dd"))
+
+  "IncorporationCheckAPIConnector" should {
+    "process a full response as expected" in {
+      stubGet("/internal/check-submission.*", 200, s"""{
+                                                      |"items":[{
+                                                      |   "company_number":"c",
+                                                      |   "transaction_status":"s",
+                                                      |   "transaction_type":"incorporation",
+                                                      |   "company_profile_link":"http://api.companieshouse.gov.uk/company/9999999999",
+                                                      |   "transaction_id":"t",
+                                                      |   "incorporated_on":"2017-05-06",
+                                                      |   "timepoint":"tp"
+                                                      | }],"links":{"next":"foo"}
+                                                      |}""".stripMargin
+      )
+
+      val expected = SubmissionCheckResponse(
+        Seq(IncorpUpdate("t","s",Some("c"),Some(asDate("2017-05-06")), "tp", None)),
+        "foo"
+      )
+      val actual = await(IncorporationCheckAPIConnector.checkSubmission(None))
+
+      actual shouldBe expected
+    }
+
+    "process a minimal single response" in {
+      stubGet("/internal/check-submission.*", 200,
+        s"""{
+           |"items":[
+           |  {"transaction_status":"s", "transaction_id":"t", "timepoint":"tp"}
+           | ],"links":{"next":"foo"}
+           |}""".stripMargin
+      )
+
+      val expected = SubmissionCheckResponse(
+        Seq(IncorpUpdate("t","s",None,None, "tp", None)),
+        "foo"
+      )
+      val actual = await(IncorporationCheckAPIConnector.checkSubmission(None))
+
+      actual shouldBe expected
+    }
+
+    "process a couple of results" in {
+      stubGet("/internal/check-submission.*", 200,
+        s"""{
+          |"items":[
+          |  {"transaction_status":"s1", "transaction_id":"t1", "timepoint":"tp1"},
+          |  {"transaction_status":"s2", "transaction_id":"t2", "timepoint":"tp2"}
+          | ],"links":{"next":"foo"}
+          |}""".stripMargin
+      )
+
+      val expected = SubmissionCheckResponse(Seq(
+        IncorpUpdate("t1","s1",None,None, "tp1", None),
+        IncorpUpdate("t2","s2",None,None, "tp2", None)
+      ), "foo"
+      )
+      val actual = await(IncorporationCheckAPIConnector.checkSubmission(None))
+
+      actual shouldBe expected
+    }
+
+    "process with no results" in {
+      stubGet("/internal/check-submission.*", 200, """{"items":[],"links":{"next":"foo"}}""")
+
+      val expected = SubmissionCheckResponse( Seq(), "foo" )
+      val actual = await(IncorporationCheckAPIConnector.checkSubmission(None))
+
+      actual shouldBe expected
+    }
+
+  }
+}
