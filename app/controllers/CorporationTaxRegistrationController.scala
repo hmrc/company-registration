f8dfa5b3542fe0faa56786c4ce409d050451843b
Chris Poole <christopher.poole@digital.hmrc.gov.uk>
2018-01-02 08:46:18 +0000

    Updated copyright to 2018

--- app/controllers/CorporationTaxRegistrationController.scala
+++ app/controllers/CorporationTaxRegistrationController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -207,8 +207,3 @@ trait CorporationTaxRegistrationController extends BaseController with Authentic
   }
 
 }
-
-
-
-
-
--- app/repositories/Repositories.scala
+++ app/repositories/Repositories.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,4 +29,3 @@ object Repositories {
   lazy val heldSubmissionRepository = new HeldSubmissionMongoRepository(mongo)
   lazy val lockRepository = new LockRepository()
 }
-
