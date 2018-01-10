09e9ac991fb66c6fd0428ee1d54be84644e88619
Chris Poole <christopher.poole@digital.hmrc.gov.uk>
2018-01-10 13:39:18 +0000

    Add log topup action & copyright

--- app/controllers/ProcessIncorporationsController.scala
+++ app/controllers/ProcessIncorporationsController.scala
@@ -19,6 +19,7 @@ package controllers
 import javax.inject.Inject
 
 import models._
+import play.api.Logger
 import play.api.libs.json.JsValue
 import play.api.mvc.Action
 import services.{MetricsService, RegistrationHoldingPenService}
@@ -41,8 +42,13 @@ trait ProcessIncorporationsController extends BaseController {
       withJsonBody[IncorpStatus]{ incorp =>
         regHoldingPenService.updateIncorp(incorp) map {
           if(_) Ok else BadRequest
-        }
+        } recover {
+          case e =>
+            Logger.error("FAILED_DES_TOPUP")
+            Logger.info(s"[ProcessIncorporationsController] [processIncorp] Topup failed for transaction ID: ${incorp.transactionId}")
+            throw e
       }
+    }
   }
 
   def processAdminIncorp = Action.async[JsValue](parse.json) {
