3198c2061b3bf53a2aadec874f61955badd58184
Daveo-G <Daveo-G@users.noreply.github.com>
2024-03-27 14:05:56 +0000

    Remove £ from threshold value

--- app/utils/CurrencyFormatter.scala
+++ app/utils/CurrencyFormatter.scala
@@ -17,7 +17,7 @@
 package utils
 
 trait CurrencyFormatter {
-  def currencyFormat(amt: BigDecimal): String = f"£$amt%,1.2f".replace(".00","")
+  def currencyFormat(amt: BigDecimal): String = f"$amt%,1.2f".replace(".00","")
 }
 
 object CurrencyFormatter extends CurrencyFormatter
